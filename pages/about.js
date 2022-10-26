2f7eebdacf875d209244fd472420fb025c5a1ec6
RevantCI <revantcherian@gmail.com>
2022-10-26 06:07:48 +0530

    update next to 13

--- pages/about.js
+++ pages/about.js
@@ -1,33 +1,28 @@
 import Head from "next/head";
-import Image from "next/image";
-import Link from "next/link";
-import styles from "../styles/Home.module.css";
-
-export default function Home() {
+const About = () => {
   return (
     <>
       <Head>
-        <title>Ninja List | Home</title>
+        <title>Ninja List | About</title>
         <meta name="keywords" content="ninjas"></meta>
       </Head>
       <div>
-        <h1 className={styles.title}>Homepage</h1>
-        <p className={styles.text}>
+        <h1>About</h1>
+        <p>
           Lorem, ipsum dolor sit amet consectetur adipisicing elit. Eius error
           ducimus earum illo officia sapiente, animi illum perspiciatis
           perferendis ea, aperiam adipisci nemo, laborum magnam nesciunt! Iure
           quam unde earum?
         </p>
-        <p className={styles.text}>
+        <p>
           Lorem, ipsum dolor sit amet consectetur adipisicing elit. Eius error
           ducimus earum illo officia sapiente, animi illum perspiciatis
           perferendis ea, aperiam adipisci nemo, laborum magnam nesciunt! Iure
           quam unde earum?
         </p>
-        <Link href="/ninjas" className={styles.btn}>
-          See Ninja Listing
-        </Link>
       </div>
     </>
   );
-}
+};
+
+export default About;
